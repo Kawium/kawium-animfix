16b2afc47956f5ae09a0510ad44cb692adfa95a6
Kawan <kawanse0@gmail.com>
2021-11-20 02:07:09 +0100

    first commit

---
+++ client/client.lua
@@ -0,0 +1,25 @@
+RegisterCommand("animfix", function(source, args, rawCommand)
+    local ped = PlayerPedId()
+    
+
+    if not IsEntityInAir(ped) then
+        if not IsPedRagdoll(ped) then
+            if not IsPedInAnyVehicle(ped) then
+                ClearPedTasksImmediately(ped)
+            
+                SetPedCanPlayGestureAnims(ped, true)
+            else
+                exports.pNotify:SendNotification({text = 'You can only use this command when outside the vehicle.', timeout = 5500, layout = "bottomCenter"})
+            end
+        else
+            exports.pNotify:SendNotification({text = 'You can only use this command while standing.', timeout = 5500, layout = "bottomCenter"})
+        end
+    else
+        exports.pNotify:SendNotification({text = 'You can only use this command while on the ground.', timeout = 5500, layout = "bottomCenter"})
+    end
+
+end, false)
+
+Citizen.CreateThread(function()
+    TriggerEvent('chat:addSuggestion', '/animfix', 'Use this command if you\'re stuck somehow.')
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "Kawium"
+
+client_script "client/client.lua"
